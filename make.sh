880b949179988bc75ef35ce202b2803dedfa2a7e
Jesse Hughes <revelstokesnowboard@gmail.com>
2017-05-08 12:48:13 -0700

    Proliferate GameState struct to supersede passing World, Camera, Plat ..  etc

--- make.sh
+++ make.sh
@@ -2,12 +2,16 @@
 
 if [ $WIN32 -eq 1 ]; then
 
+  echo " -- Building Win32"
+
   BINARY="bin/Debug/Bonsai.exe"
   msbuild.exe /nologo /v:m ./bin/Game.vcxproj
 
-  cp ./bin/Debug/Game.dll ./bin/Debug/GameLoadable.dll
+  cp bin/Debug/Game.dll bin/Debug/GameLoadable.dll
 
 else # Unix
+
+  echo " -- Building Unix"
 
   BINARY="bin/Bonsai"
 
